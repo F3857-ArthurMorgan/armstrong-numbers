fc14f3192b88c379d4f6083e0683c3c128b3de85
F3857-ArthurMorgan <106927466+F3857-ArthurMorgan@users.noreply.github.com>
2022-06-05 22:30:47 +0300

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,20 +1,20 @@
-
-def a():
-print("ARMSTRONG NUMARASI BULMAYA HOŞ GELDİN, DEVAM ETMEK ENTER'A BAS")
-input()
-sayi = int(input("Sayıyı Giriniz:"))
-basamak = str(sayi)
-toplam=0
-for x in basamak:
-    rakam = int(x)**len(basamak)
-    toplam += rakam
-if(sayi == toplam):
-    print("Bu Bir Armstrong Sayısıdır.")
-else:
-    print("Armstong Sayısı Degildir.")
-
-print("tekrar oynamak için yes veya no")
-yanıt=input()
-if  ( yanıt== yes, Yes, YES):
-    while() == "tekrarla":
-        def a:
+while True:
+    print("ARMSTRONG NUMARASI BULMAYA HOŞ GELDİN, DEVAM ETMEK ENTER'A BAS")
+    input()
+    sayi = int(input("Sayıyı Giriniz:"))
+    basamak = str(sayi)
+    toplam=0
+    for x in basamak:
+        rakam = int(x)**len(basamak)
+        toplam += rakam
+    if(sayi == toplam):
+        print("Bu Bir Armstrong Sayısıdır.")
+    else:
+        print("Armstong Sayısı Degildir.")
+    print("tekrar oynamak için yes veya no")
+    yanıt=input()
+    if yanıt=="yes":
+        continue
+    else:
+        print("oynadığın için teşekkürler")
+        break
